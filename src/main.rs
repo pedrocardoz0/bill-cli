3dc9ea40f0e28d570e34e3f3d664deb5ea12297c
Pedro Cardozo <p-cardozo@hotmail.com>
2022-09-29 09:09:43 -0300

    fix: scope vector

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use bill::{add, display_menu, get_input, Bill};
 use std::{thread, time::Duration};
 
 fn main() {
-    loop {
-        let mut bills: Vec<Bill> = Vec::new();
+    let mut bills: Vec<Bill> = Vec::new();
 
+    loop {
         display_menu();
 
         let input_data = get_input();
@@ -14,17 +14,17 @@ fn main() {
             Err(e) => break,
         };
 
-        bills.push(Bill::new("cu".to_string(), 2.0));
-        let new_bill = if input_choice == 1 {
-            add()
-        } else if input_choice == 2 {
+        if input_choice == 1 {
+            let new_bill = add();
+            dbg!(&new_bill);
+            bills.push(new_bill);
+        }
+
+        if input_choice == 2 {
             println!("== Bils == ");
             println!("{:?}", bills);
             thread::sleep(Duration::from_millis(4000));
-            return ();
-        } else {
-            return ();
-        };
+        }
 
         println!("input_choice >> {:?}", input_choice);
 
